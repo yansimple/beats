af4d96e33197f29754627247f9bf514828248640
Данил Янзин <danilyanzin@MacBook-Pro-Danil.local>
2023-05-20 18:21:21 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 const leftBtn = document.querySelector("#leftArrow")
 const rightBtn = document.querySelector("#rightArrow")
-const items = document.querySelector("#item-price")
+const items = document.querySelector("#item-switcher")
 
 let currentRight = 0
 let currentLeft = 0
 rightBtn.addEventListener("click", e => {
     e.preventDefault()
     currentRight += 915;
-    items.style.right = "${currentRight}px"
+    items.style.right = '${currentRight}px';
 })
 
 leftBtn.addEventListener("click", e => {
     e.preventDefault()
     currentLeft -= 915
     
-    currentLeft.style.left("${currentLeft}px")
+    items.style.left = '${currentLeft}px';
 })
